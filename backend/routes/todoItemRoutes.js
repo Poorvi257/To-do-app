5ee73ebb87d30e2e88c78990f8e743a05c842532
Poorvi Shrivastava <poorvishrivastava@Poorvis-MacBook-Pro.local>
2023-08-12 01:57:26 +0530

    Update todo item routes adding comments

--- backend/routes/todoItemRoutes.js
+++ backend/routes/todoItemRoutes.js
@@ -1,12 +1,29 @@
-const app = require("express");
-const { createTodoItemTable, createItem, showItems, updateItem, deleteItem } = require("../controllers/todoItems");
-const router = app.Router()
+const express = require("express"); 
+const {
+    createTodoItemTable,  
+    createItem,          
+    showItems,           
+    updateItem,          
+    deleteItem           
+} = require("../controllers/todoItems"); 
 
+const router = express.Router(); 
 
-router.get('/create/todoItemTable', createTodoItemTable)
-router.post('/addItem/:id', createItem)
-router.get('/get/todoItems/:id', showItems)
-router.put('/updateItem/:id', updateItem)
-router.delete('/deleteItem/:id', deleteItem)
+// Define routes
 
-module.exports = router
+// Route to create the 'todoItems' table
+router.get('/create/todoItemTable', createTodoItemTable);
+
+// Route to create a new todo item associated with a specific list ID
+router.post('/addItem/:id', createItem);
+
+// Route to retrieve todo items by list ID
+router.get('/get/todoItems/:id', showItems);
+
+// Route to update a todo item by its ID
+router.put('/updateItem/:id', updateItem);
+
+// Route to delete a todo item by its ID
+router.delete('/deleteItem/:id', deleteItem);
+
+module.exports = router;
