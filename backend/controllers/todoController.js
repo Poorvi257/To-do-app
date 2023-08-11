0299d151affcdb1c5808a350bceb771e6fc5c6a2
Poorvi Shrivastava <poorvishrivastava@Poorvis-MacBook-Pro.local>
2023-08-12 01:41:18 +0530

    Update Todo controller with error handling

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,70 +1,117 @@
-const db = require('../db/database')
+const db = require('../db/database');
 
-exports.createTable = (req, res) => {  
-    var sql = `CREATE TABLE todos (id INT AUTO_INCREMENT, 
-                                    listName VARCHAR(255), 
-                                    userId INT, 
-                                    timestamp TIMESTAMP, 
-                                    PRIMARY KEY(id), 
-                                    FOREIGN KEY (userId) REFERENCES user(id)
-                                )`;  
-    db.query(sql, (err, result) => {  
-    if (err) throw err;  
-    return res.status(201).json("Table created");
-    });  
-};  
+// Function to create the 'todos' table
+exports.createTable = (req, res) => {
+    // SQL query to create the 'todos' table with necessary columns and foreign key reference
+    var sql = `CREATE TABLE IF NOT EXISTS todos (
+        id INT AUTO_INCREMENT,
+        listName VARCHAR(255),
+        userId INT,
+        timestamp TIMESTAMP,
+        PRIMARY KEY(id),
+        FOREIGN KEY (userId) REFERENCES user(id)
+    )`;
 
-exports.createList = (req, res) => {  
-//check if the user exists
-    var sql = "INSERT INTO todos SET ?";  
-    const userId = req.params.id
-    const timestamp = new Date()
-    const {listName} = req.body
-
-    db.query(sql, {listName, userId, timestamp}, (err, result) => {  
-    if (err) return res.json(err);  
-    return res.status(200).json(result);
-    });  
-};  
-
-exports.showList = (req, res) => {  
-    var sql = "SELECT * FROM todos";  
-
-    db.query(sql, (err, result) => {  
-    if (err) return res.json(err);  
-    return res.status(200).json(result);
-    });  
-};  
-
-exports.showSingleList = (req, res) => {  
-    //check if user exists and send msg accordingly
-    var sql = `SELECT * FROM todos where userId=${req.params.id}`;  
-
-    db.query(sql, (err, result) => {  
-    if (err) return res.json(err);  
-    return res.status(200).json(result[0]);
-    });  
+    // Execute the SQL query to create the table
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json("Table creation failed");
+        }
+        return res.status(201).json("Table created");
+    });
 };
 
-exports.updateRecord = (req, res) => {  
-    //check if the list exists
-    var sql = `UPDATE todos SET listName=?, timestamp=? where id=${req.params.id}`;  
-    
-    const {listName} = req.body
+// Function to create a new list
+exports.createList = (req, res) => {
+    // SQL query to insert a new list into the 'todos' table
+    var sql = "INSERT INTO todos SET ?";
 
-    db.query(sql, [listName, new Date()], (err, result) => {  
-    if (err) return res.json(err);  
-    return res.status(200).json(result);
-    });  
-}; 
+    const userId = req.params.id;
+    const timestamp = new Date();
+    const { listName } = req.body;
 
-exports.deleteRecord = (req, res) => { 
-     //check if the list exists
- 
-    var sql = `DELETE FROM todos where id=${req.params.id}`;  
+    // Data to be inserted into the table
+    const newListData = {
+        listName,
+        userId,
+        timestamp
+    };
 
-    db.query(sql, (err, result) => {  
-    if (err) return res.json(err);  
-    return res.status(200).json("Record Deleted");
-    });  
+    // Execute the SQL query to insert the new list
+    db.query(sql, newListData, (err, result) => {
+        if (err) {
+            return res.status(500).json("List creation failed");
+        }
+        return res.status(200).json(result);
+    });
+};
+
+// Function to retrieve all lists
+exports.showList = (req, res) => {
+    // SQL query to select all lists
+    var sql = "SELECT * FROM todos";
+
+    // Execute the SQL query to retrieve lists
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json("Failed to retrieve lists");
+        }
+        return res.status(200).json(result);
+    });
+};
+
+// Function to retrieve a single list for a specific user
+exports.showSingleList = (req, res) => {
+    // SQL query to select a single list associated with a specific user
+    var sql = `SELECT * FROM todos WHERE userId=${req.params.id}`;
+
+    // Execute the SQL query to retrieve the single list
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json("Failed to retrieve list");
+        }
+        if (result.length === 0) {
+            // No list found for the specified user
+            return res.status(404).json("List not found");
+        }
+        return res.status(200).json(result[0]);
+    });
+};
+
+// Function to update a list's information
+exports.updateRecord = (req, res) => {
+    // SQL query to update a list's information based on its ID
+    var sql = `UPDATE todos SET listName=?, timestamp=? WHERE id=${req.params.id}`;
+
+    const { listName } = req.body;
+
+    // Execute the SQL query to update the list
+    db.query(sql, [listName, new Date()], (err, result) => {
+        if (err) {
+            return res.status(500).json("Failed to update list");
+        }
+        if (result.affectedRows === 0) {
+            // No rows were affected, meaning the list with the specified ID was not found
+            return res.status(404).json("List not found");
+        }
+        return res.status(200).json(result);
+    });
+};
+
+// Function to delete a list
+exports.deleteRecord = (req, res) => {
+    // SQL query to delete a list based on its ID
+    var sql = `DELETE FROM todos WHERE id=${req.params.id}`;
+
+    // Execute the SQL query to delete the list
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json("Failed to delete list");
+        }
+        if (result.affectedRows === 0) {
+            // No rows were affected, meaning the list with the specified ID was not found
+            return res.status(404).json("List not found");
+        }
+        return res.status(200).json("Record Deleted");
+    });
 };
