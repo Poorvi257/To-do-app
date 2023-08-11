75e6627d9ac7d1342ad3c3bd3fd1fa32bed448ec
Poorvi Shrivastava <poorvishrivastava@Poorvis-MacBook-Pro.local>
2023-08-12 02:07:37 +0530

    Adding more comments

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// import routes
 const user = require('./routes/userRoutes')
 const todos = require('./routes/todoRoutes') 
 const todoItems = require('./routes/todoItemRoutes')
+const migration = require('./routes/migrationRoutes')
 
+//MIDDLEWARE
 app.use(morgan('dev'))
 app.use(bodyParser.json({limit: "5mb"}))
 app.use(bodyParser.urlencoded({
@@ -16,10 +19,14 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(cors())
 
+
+//ROUTES MIDDLEWARE
 app.use('/api', user)
 app.use('/api', todos)
 app.use('/api', todoItems)
+app.use('/api', migration)
 
+//port
 const port = 8000
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`)
